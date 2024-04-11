9ff739b0522fc61cb6bbb361207ca1678a27614b
ShinYoung-Kim <lukey0515@naver.com>
2024-04-11 22:15:47 +0900

    feat: color, typo constant 생성

---
+++ constants/colors.ts
@@ -0,0 +1,19 @@
+export const colors = {
+	'Purple-100': '#F1E5FF',
+	'Purple-200': '#E3CCFF',
+	'Purple-300': '#D2B2FF',
+	'Purple-400': '#A875FF',
+	'Purple-500': '#7D40FF',
+	'Purple-600': '#671ADB',
+	'Purple-700': '#432499',
+	'Purple-800': '#2E2159',
+	White: '#FFFFFF',
+	'Gray-100': '#F3F3F3',
+	'Gray-200': '#E8E8E8',
+	'Gray-300': '#BBBBBB',
+	'Gray-400': '#787878',
+	'Gray-500': '#1E1E1E',
+};
+
+export type Color = keyof typeof colors;
+export type ColorValue = (typeof colors)[Color];
---
+++ constants/fonts.ts
@@ -0,0 +1,72 @@
+export const fontUrl = 'https://cdn.jsdelivr.net/gh/sun-typeface/SUIT/fonts/static/woff2/SUIT.css';
+
+export const fonts = {
+	Header1: {
+		fontWeight: 'bold',
+		fontSize: '44px',
+	},
+	Header2: {
+		fontWeight: 'bold',
+		fontSize: '40px',
+	},
+	Header3: {
+		fontWeight: 'bold',
+		fontSize: '36px',
+	},
+	Header4: {
+		fontWeight: 'bold',
+		fontSize: '32px',
+	},
+	Header5: {
+		fontWeight: 'bold',
+		fontSize: '28px',
+	},
+	Header6: {
+		fontWeight: 'bold',
+		fontSize: '24px',
+	},
+	SubHeader1: {
+		fontWeight: 'bold',
+		fontSize: '20px',
+	},
+	SubHeader2: {
+		fontWeight: 'medium',
+		fontSize: '20px',
+	},
+	SubHeader3: {
+		fontWeight: 'bold',
+		fontSize: '16px',
+	},
+	Body1: {
+		fontWeight: 'medium',
+		fontSize: '16px',
+	},
+	Body2: {
+		fontWeight: 'regular',
+		fontSize: '16px',
+	},
+	Body3: {
+		fontWeight: 'medium',
+		fontSize: '14px',
+	},
+	Body4: {
+		fontWeight: 'regular',
+		fontSize: '14px',
+	},
+	Caption1: {
+		fontWeight: 'medium',
+		fontSize: '12px',
+	},
+	Caption2: {
+		fontWeight: 'regular',
+		fontSize: '12px',
+	},
+	Caption3: {
+		fontWeight: 'medium',
+		fontSize: '10px',
+	},
+	Caption4: {
+		fontWeight: 'regular',
+		fontSize: '10px',
+	},
+};
