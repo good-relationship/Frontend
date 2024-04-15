ecf6fc6026c3b3c1f1e01e3e9b4218564884813a
ShinYoung-Kim <lukey0515@naver.com>
2024-04-16 00:59:44 +0900

    rename: typo const 이름 수정 및 fontWeight 변경

--- constants/typos.ts
+++ constants/typos.ts
@@ -1,72 +1,70 @@
-export const fontUrl = 'https://cdn.jsdelivr.net/gh/sun-typeface/SUIT/fonts/static/woff2/SUIT.css';
-
-export const fonts = {
+export const typos = {
 	'.typo-Header1': {
-		fontWeight: 'bold',
+		fontWeight: '700',
 		fontSize: '44px',
 	},
 	'.typo-Header2': {
-		fontWeight: 'bold',
+		fontWeight: '700',
 		fontSize: '40px',
 	},
 	'.typo-Header3': {
-		fontWeight: 'bold',
+		fontWeight: '700',
 		fontSize: '36px',
 	},
 	'.typo-Header4': {
-		fontWeight: 'bold',
+		fontWeight: '700',
 		fontSize: '32px',
 	},
 	'.typo-Header5': {
-		fontWeight: 'bold',
+		fontWeight: '700',
 		fontSize: '28px',
 	},
 	'.typo-Header6': {
-		fontWeight: 'bold',
+		fontWeight: '700',
 		fontSize: '24px',
 	},
 	'.typo-SubHeader1': {
-		fontWeight: 'bold',
+		fontWeight: '700',
 		fontSize: '20px',
 	},
 	'.typo-SubHeader2': {
-		fontWeight: 'medium',
+		fontWeight: '500',
 		fontSize: '20px',
 	},
 	'.typo-SubHeader3': {
-		fontWeight: 'bold',
+		fontWeight: '700',
 		fontSize: '16px',
 	},
 	'.typo-Body1': {
-		fontWeight: 'medium',
+		fontWeight: '500',
 		fontSize: '16px',
 	},
 	'.typo-Body2': {
-		fontWeight: 'regular',
+		fontWeight: '400',
 		fontSize: '16px',
 	},
 	'.typo-Body3': {
-		fontWeight: 'medium',
+		fontWeight: '500',
 		fontSize: '14px',
 	},
 	'.typo-Body4': {
-		fontWeight: 'regular',
+		fontWeight: '400',
 		fontSize: '14px',
 	},
 	'.typo-Caption1': {
-		fontWeight: 'medium',
+		fontWeight: '500',
 		fontSize: '12px',
 	},
 	'.typo-Caption2': {
-		fontWeight: 'regular',
+		fontWeight: '400',
 		fontSize: '12px',
 	},
 	'.typo-Caption3': {
-		fontWeight: 'medium',
+		fontWeight: '500',
 		fontSize: '10px',
 	},
 	'.typo-Caption4': {
-		fontWeight: 'regular',
+		fontWeight: '400',
 		fontSize: '10px',
 	},
 };
