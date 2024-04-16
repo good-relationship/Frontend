ffba5684b61aa546d229cfa44beeecbb475f730a
ShinYoung-Kim <lukey0515@naver.com>
2024-04-17 07:42:32 +0900

    feat: 사이드바 컴포넌트 구현

---
+++ components/sidebar/SidebarLink.tsx
@@ -0,0 +1,36 @@
+import Link from 'next/link';
+
+import Icon from '@/components/Icon';
+import { colors } from '@/constants/colors';
+import { cn } from '@/lib/utils';
+import { IconId } from '@/types/icons';
+
+type SidebarLinkProps = {
+	label: string;
+	path: string;
+	icon: IconId;
+	isActive: boolean;
+};
+
+const SidebarLink = ({ label, path, icon, isActive }: SidebarLinkProps) => {
+	return (
+		<Link
+			href={path}
+			className={cn('flex gap-[10px] rounded-[10px] typo-SubHeader3 px-5 py-[15px]', {
+				'!text-Purple-500 bg-Purple-100': isActive,
+			})}
+		>
+			{/* <Image
+				src={icon}
+				alt={label}
+				width={20}
+				height={20}
+				className={`${isActive ? 'fill-Purple-500' : 'fill-Gray-400'}`}
+			/> */}
+			<Icon id={icon} color={isActive ? colors['Purple-500'] : colors['Gray-400']} size={20} />
+			<span>{label}</span>
+		</Link>
+	);
+};
+
+export default SidebarLink;
