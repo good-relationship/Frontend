4d63c0b0d7a16fc06a3e78aa309fcac3cde28c76
ShinYoung-Kim <lukey0515@naver.com>
2024-04-17 07:42:05 +0900

    feat: 사이드바 라우팅 상수 생성

---
+++ constants/routings.ts
@@ -0,0 +1,22 @@
+export const sidebarRoutings = [
+	{
+		label: '회의하기',
+		path: '/meeting',
+		icon: 'webcam',
+	},
+	{
+		label: '기록하기',
+		path: '/document',
+		icon: 'pencil',
+	},
+	{
+		label: '일정 관리',
+		path: '/schedule',
+		icon: 'calendar',
+	},
+	{
+		label: '팀스페이스 설정',
+		path: '/setting',
+		icon: 'setting',
+	},
+];
