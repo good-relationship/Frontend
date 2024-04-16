c9ad78295ab1f1e1bc38afe8fdf1b8c40dc2315a
ShinYoung-Kim <lukey0515@naver.com>
2024-04-17 08:47:26 +0900

    design: rounded button 컴포넌트 디자인 수정

--- components/RoundedButton.tsx
+++ components/RoundedButton.tsx
@@ -12,16 +12,14 @@ interface RoundedButtonProps extends VariantProps<typeof buttonVariants> {
 const buttonVariants = cva('rounded-[100px] h-fit w-fit', {
 	variants: {
 		size: {
-			Small: 'gap-[10px] px-6 py-2 !typo-Body3',
-			Medium: 'gap-2 px-6 py-4 !typo-Body1',
-			Large: 'gap-[10px] px-8 py-3 !typo-SubHeader2',
+			Small: 'gap-[10px] px-6 py-2 !typo-Body3 h-9',
+			Medium: 'gap-2 px-6 py-4 !typo-Body1 h-10',
+			Large: 'gap-[10px] px-8 py-3 !typo-SubHeader2 h-[56px]',
 		},
 		variant: {
-			Black: 'bg-black text-white hover:text-Purple-300',
-			Purple: 'bg-Purple-300 text-black hover:bg-Purple-300',
-			Outline: 'border border-Gray-400 text-Gray-400 bg-white hover:bg-white',
-			Disabled: 'bg-Gray-100 text-Gray-300 hover:bg-Gray-100',
-			'Outline-Disabled': 'border border-Gray-200 text-Gray-300 bg-white hover:bg-white',
+			Black: 'bg-black text-white hover:text-Purple-300 disabled:bg-Gray-100 disabled:text-Gray-300',
+			Purple: 'bg-Purple-300 text-black hover:bg-Purple-300 disabled:bg-Gray-100 disabled:text-Gray-300',
+			Outline: 'border border-Gray-400 text-Gray-400 bg-white hover:bg-white disabled:text-Gray-300',
 		},
 	},
 	defaultVariants: {
