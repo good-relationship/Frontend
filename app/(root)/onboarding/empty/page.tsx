0daa343de84acef5942bbd647b6e889c9c5524fe
ShinYoung-Kim <lukey0515@naver.com>
2024-04-16 18:46:39 +0900

    feat: onboarding 페이지들 생성

---
+++ app/(root)/onboarding/empty/page.tsx
@@ -0,0 +1,5 @@
+const NoWorkspace = () => {
+	return <div>소속된 워크스페이스가 없습니다.</div>;
+};
+
+export default NoWorkspace;
---
+++ app/(root)/onboarding/overflow/page.tsx
@@ -0,0 +1,5 @@
+const WorkspaceOverflow = () => {
+	return <div>참여중인 워크스페이스가 있습니다.</div>;
+};
+
+export default WorkspaceOverflow;
