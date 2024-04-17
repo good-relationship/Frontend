c2b7e027e0318b6f12458d9e99e254ffc72f743b
ShinYoung-Kim <lukey0515@naver.com>
2024-04-17 18:59:11 +0900

    design: nav 컴포넌트 디자인 변경

--- components/onboarding/Nav.tsx
+++ components/onboarding/Nav.tsx
@@ -9,7 +9,7 @@ const Nav = ({ title, description }: NavProps) => {
 	return (
 		<nav className="flex flex-col gap-1 items-center">
 			<Image src="/icons/kan_text_vertical.svg" width={50} height={50} alt="Kan Logo" />
-			<div className="flex flex-col gap-[15px] items-center">
+			<div className="flex flex-col gap-[15px] items-center whitespace-pre-line text-center">
 				<h1 className="typo-Header1 text-Gray-500">{title}</h1>
 				<p className="typo-SubHeader3 text-Gray-400">{description}</p>
 			</div>
