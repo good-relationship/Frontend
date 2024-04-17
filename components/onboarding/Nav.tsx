714edd5546b21e96c071f8d242417f4e00b5ed4d
ShinYoung-Kim <lukey0515@naver.com>
2024-04-17 19:48:50 +0900

    feat: nav 컴포넌트에서 로고 이미지 제거

--- components/onboarding/Nav.tsx
+++ components/onboarding/Nav.tsx
@@ -1,5 +1,3 @@
-import Image from 'next/image';
-
 type NavProps = {
 	title: string;
 	description?: string;
@@ -8,7 +6,6 @@ type NavProps = {
 const Nav = ({ title, description }: NavProps) => {
 	return (
 		<nav className="flex flex-col gap-1 items-center">
-			<Image src="/icons/kan_text_vertical.svg" width={50} height={50} alt="Kan Logo" />
 			<div className="flex flex-col gap-[15px] items-center whitespace-pre-line text-center">
 				<h1 className="typo-Header1 text-Gray-500">{title}</h1>
 				<p className="typo-SubHeader3 text-Gray-400">{description}</p>
