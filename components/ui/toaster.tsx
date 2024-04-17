0128f5a638896e90459062c88ce641f5fea9d1b2
ShinYoung-Kim <lukey0515@naver.com>
2024-04-17 16:14:18 +0900

    chore: toast 설치

---
+++ components/ui/toaster.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import { Toast, ToastProvider, ToastTitle, ToastViewport } from '@/components/ui/toast';
+import { useToast } from '@/components/ui/use-toast';
+
+export function Toaster() {
+	const { toasts } = useToast();
+
+	return (
+		<ToastProvider>
+			{toasts.map(function ({ id, title, action, ...props }) {
+				return (
+					<Toast key={id} {...props}>
+						<div className="grid gap-1">{title && <ToastTitle>{title}</ToastTitle>}</div>
+						{action}
+					</Toast>
+				);
+			})}
+			<ToastViewport />
+		</ToastProvider>
+	);
+}
