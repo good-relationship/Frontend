e3e547bef697377d63a01cf6721a9f239ebf4ad2
ShinYoung-Kim <lukey0515@naver.com>
2024-04-17 17:48:32 +0900

    design: sidebar에 custom drop shadow 적용

--- components/sidebar/Sidebar.tsx
+++ components/sidebar/Sidebar.tsx
@@ -9,7 +9,7 @@ import { sidebarRoutings } from '@/constants/routings';
 const Sidebar = () => {
 	const pathname = usePathname();
 	return (
-		<aside className="w-[296px] h-full py-[10px] px-4">
+		<aside className="w-[296px] h-full py-[10px] px-4 custom-shadow bg-White">
 			<SidebarTitle />
 			{sidebarRoutings.map(({ label, path, icon }) => {
 				const isActive = pathname === path || pathname.startsWith(`${path}/`);
