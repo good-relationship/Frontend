588464d8c8747bd757e198545cd029b766642603
ShinYoung-Kim <lukey0515@naver.com>
2024-04-19 18:00:58 +0900

    feat: search input value와 버튼 연결

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,5 +1,7 @@
+'use client';
+
 import Image from 'next/image';
-import { InputHTMLAttributes } from 'react';
+import { ChangeEvent, InputHTMLAttributes, KeyboardEvent, useState } from 'react';
 
 import SquareButton from '@/components/SquareButton';
 import { Input } from '@/components/ui/input';
@@ -7,13 +9,34 @@ import { Input } from '@/components/ui/input';
 interface SearchInputProps extends InputHTMLAttributes<HTMLInputElement> {}
 
 const SearchInput = ({ placeholder }: SearchInputProps) => {
+	const [value, setValue] = useState('');
+	const handleInputValueChange = (e: ChangeEvent<HTMLInputElement>) => {
+		setValue(e.target.value);
+	};
+	// TODO: 검색 기능과 event 핸들러 연결
+	const handleOnKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+		if (e.key === 'Enter') {
+			console.log('Enter key pressed');
+		}
+	};
+
+	const isInputEmpty = value === '';
+
 	return (
 		<div className="flex bg-Gray-100 px-5 h-[72px] items-center rounded-xl w-full">
 			<div className="flex gap-5 flex-1">
 				<Image src="/icons/search.svg" width={36} height={36} alt="search icon" />
-				<Input className="bg-Gray-100" placeholder={placeholder} />
+				<Input
+					className="bg-Gray-100"
+					placeholder={placeholder}
+					value={value}
+					onChange={handleInputValueChange}
+					onKeyDown={handleOnKeyDown}
+				/>
 			</div>
-			<SquareButton size="Small">검색</SquareButton>
+			<SquareButton size="Small" disabled={isInputEmpty}>
+				검색
+			</SquareButton>
 		</div>
 	);
 };
