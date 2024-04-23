5b36c031e000abc26ddb0381beefa1e32b6b8b77
ShinYoung-Kim <lukey0515@naver.com>
2024-04-23 16:08:47 +0900

    feat: 글자수 세는 input 추가

--- components/onboarding/create/EnterWorkspaceNameTemplate.tsx
+++ components/onboarding/create/EnterWorkspaceNameTemplate.tsx
@@ -1,10 +1,15 @@
+import CountInput from '@/components/CountInput';
 import CreateProgressNav from '@/components/onboarding/create/CreateProgressNav';
+import SquareButton from '@/components/SquareButton';
 
 const EnterWorkspaceNameTemplate = () => {
 	return (
 		<main className="flex-col-template">
 			<CreateProgressNav />
-			workspace 정하기
+			<div className="w-full h-full flex flex-col gap-5 items-end">
+				<CountInput className="w-full" label="팀명" countLimit={10} />
+				<SquareButton size="Small">다음</SquareButton>
+			</div>
 		</main>
 	);
 };
--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -2,7 +2,9 @@ import * as React from 'react';
 
 import { cn } from '@/lib/utils';
 
-export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+	count?: number;
+}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, type, ...props }, ref) => {
 	return (
