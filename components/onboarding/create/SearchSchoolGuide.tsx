3411701688f2bb26448096769e48be488a26ad24
ShinYoung-Kim <lukey0515@naver.com>
2024-04-24 15:02:21 +0900

    design: 학교 검색 전 layout 수정

--- components/onboarding/create/SearchSchoolGuide.tsx
+++ components/onboarding/create/SearchSchoolGuide.tsx
@@ -2,10 +2,12 @@ import Image from 'next/image';
 
 const SearchSchoolGuide = () => {
 	return (
-		<div className="flex flex-col items-center">
-			<Image src="/images/e_commerce.png" alt="e_commerce" width="210" height="210" />
-			<p className="typo-SubHeader3">학교 검색</p>
-			<p className="typo-Body1">재학중인 대학교 이름을 검색해주세요.</p>
+		<div className="flex items-center gap-5">
+			<Image src="/images/e_commerce.png" alt="e_commerce" width="200" height="200" />
+			<div className="flex flex-col items-start">
+				<h6 className="typo-Header6 text-Gray-400">학교 검색</h6>
+				<p className="typo-Body1 text-Gray-400">재학중인 대학교 이름을 검색해주세요.</p>
+			</div>
 		</div>
 	);
 };
