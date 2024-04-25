9422229db8c73f688a7487a093e68f5f57040fe6
ShinYoung-Kim <lukey0515@naver.com>
2024-04-26 05:46:26 +0900

    feat: workspace name form 분리

--- components/onboarding/create/EnterWorkspaceNameTemplate.tsx
+++ components/onboarding/create/EnterWorkspaceNameTemplate.tsx
@@ -1,38 +1,12 @@
-'use client';
-
-import { useRouter } from 'next/navigation';
-
-import Form from '@/components/form/Form';
 import CreateProgressNav from '@/components/onboarding/create/CreateProgressNav';
-import SquareButton from '@/components/SquareButton';
-import { workspaceNameSchema } from '@/constants/form';
-import { FormType } from '@/types/form';
+import WorkspaceNameForm from '@/components/onboarding/create/WorkspaceNameForm';
 
 const EnterWorkspaceNameTemplate = () => {
-	const router = useRouter();
-
-	const onSubmit = (data: FormType) => {
-		console.log(data);
-	};
-
-	const handleClickNext = () => {
-		router.push(`${window.location}&workspace=workspaceName`);
-	};
-
 	return (
 		<main className="flex-col-template">
 			<CreateProgressNav />
 			<div className="w-full h-full flex flex-col gap-5 items-end">
-				<Form onSubmit={onSubmit} schema={workspaceNameSchema} className="w-full flex flex-col gap-2">
-					<Form.Label>팀명</Form.Label>
-					<Form.Input name="workspaceName" placeholder="팀명을 입력해주세요" countLimit={10} variant="Line" />
-					<Form.HelperText className="px-4" name="workspaceName" />
-					<div className="w-full flex justify-end">
-						<SquareButton size="Small" onClick={handleClickNext}>
-							다음
-						</SquareButton>
-					</div>
-				</Form>
+				<WorkspaceNameForm />
 			</div>
 		</main>
 	);
