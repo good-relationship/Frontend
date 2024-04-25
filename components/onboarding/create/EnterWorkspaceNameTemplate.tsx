667099e78a2c17a0ae5cbb16dbfa36a0f502ca7e
ShinYoung-Kim <lukey0515@naver.com>
2024-04-26 04:38:24 +0900

    feat: input 컴포넌트 팀명 입력 페이지에 적용

--- components/onboarding/create/EnterWorkspaceNameTemplate.tsx
+++ components/onboarding/create/EnterWorkspaceNameTemplate.tsx
@@ -2,12 +2,18 @@
 
 import { useRouter } from 'next/navigation';
 
-import CountInput from '@/components/CountInput';
+import Form from '@/components/form/Form';
 import CreateProgressNav from '@/components/onboarding/create/CreateProgressNav';
 import SquareButton from '@/components/SquareButton';
+import { workspaceNameSchema } from '@/constants/form';
+import { FormType } from '@/types/form';
 
 const EnterWorkspaceNameTemplate = () => {
 	const router = useRouter();
+
+	const onSubmit = (data: FormType) => {
+		console.log(data);
+	};
 
 	const handleClickNext = () => {
 		router.push(`${window.location}&workspace=workspaceName`);
@@ -17,10 +23,16 @@ const EnterWorkspaceNameTemplate = () => {
 		<main className="flex-col-template">
 			<CreateProgressNav />
 			<div className="w-full h-full flex flex-col gap-5 items-end">
-				<CountInput className="w-full" label="팀명" countLimit={10} />
-				<SquareButton size="Small" onClick={handleClickNext}>
-					다음
-				</SquareButton>
+				<Form onSubmit={onSubmit} schema={workspaceNameSchema} className="w-full flex flex-col gap-2">
+					<Form.Label>팀명</Form.Label>
+					<Form.Input name="workspaceName" placeholder="팀명을 입력해주세요" countLimit={10} variant="Line" />
+					<Form.HelperText className="px-4" name="workspaceName" />
+					<div className="w-full flex justify-end">
+						<SquareButton size="Small" onClick={handleClickNext}>
+							다음
+						</SquareButton>
+					</div>
+				</Form>
 			</div>
 		</main>
 	);
