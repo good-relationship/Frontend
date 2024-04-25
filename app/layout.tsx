0888f35f9b2fc6a2d3743af7c01bd56c477d2bbe
ShinYoung-Kim <lukey0515@naver.com>
2024-04-25 11:58:05 +0900

    design: 전체 레이아웃 높이 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 import './globals.css';
 
+import type { Metadata } from 'next';
+
 import Header from '@/components/header/Header';
 import { Toaster } from '@/components/ui/toaster';
 import RecoilRootProvider from '@/lib/recoil/RecoilRootProvider';
 import { pretendard } from '@/utils/fonts';
-
-import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
 	title: '캡스톤 디자인 맞춤 서비스 | 조은사이',
@@ -27,7 +27,7 @@ export default function RootLayout({
 			<body className={`${pretendard.variable} font-pretendard w-full flex justify-center min-h-screen`}>
 				<RecoilRootProvider>
 					<Header variant="user-info" />
-					<div className="w-full max-w-[1440px] flex-1 max-h-[calc(100vh-60px)] absolute top-[60px] overflow-auto">
+					<div className="w-full max-w-[1440px] flex-1 max-h-[calc(100vh-60px)] absolute top-[60px] h-full overflow-auto">
 						{children}
 					</div>
 					<Toaster />
