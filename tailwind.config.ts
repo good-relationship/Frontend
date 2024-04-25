313aed1a5eaed92a30cacb37d77b54f9dabe8003
Jieun Lim <jieun776121@gmail.com>
2024-04-26 02:44:26 +0900

    desgin: Pretendard global 폰트 적용

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -27,6 +27,9 @@ const config = {
 				'accordion-down': 'accordion-down 0.2s ease-out',
 				'accordion-up': 'accordion-up 0.2s ease-out',
 			},
+			fontFamily: {
+				Pretendard: ['Pretendard'],
+			},
 		},
 	},
 	plugins: [require('tailwindcss-animate')],
