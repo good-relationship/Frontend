37f84caa7a05c36e8c7fa8ec95a530e53dc30d34
ShinYoung-Kim <lukey0515@naver.com>
2024-04-26 00:33:24 +0900

    feat: invited 페이지에 인원 초과 알림 모달 적용

--- app/(root)/onboarding/invited/page.tsx
+++ app/(root)/onboarding/invited/page.tsx
@@ -2,28 +2,38 @@
 
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
+import MemberOverflowDialog from '@/components/dialog/MemberOverflowDialog';
 import Nav from '@/components/onboarding/Nav';
 import SquareButton from '@/components/SquareButton';
 import UserListItem from '@/components/UserListItem';
+import { MAX_WORKSPACE_MEMBER_COUNT } from '@/constants/workspaces';
 import { mockGetUserWorkspaceInfoData } from '@/mocks/onboarding';
 
 const InvitedWorkspace = () => {
 	const route = useRouter();
+	const [isMemberOverflowDialogOpen, setIsMemberOverflowDialogOpen] = useState(false);
 
 	// TODO: 워크스페이스 이름 받아오기
 	const workspaceName = '조은 사이 워크스페이스';
 	const title = `${workspaceName}에\n 초대되셨습니다.`;
 
 	const userList = mockGetUserWorkspaceInfoData.users;
+	const isAcceptInvitePossible = userList.length < MAX_WORKSPACE_MEMBER_COUNT;
 
 	const handleClickJoinWorkspace = () => {
+		if (!isAcceptInvitePossible) {
+			setIsMemberOverflowDialogOpen(true);
+			return;
+		}
 		// TODO: 참여 수락 API 연결
 		route.push('/document');
 	};
 
 	return (
 		<div className="flex-col-template">
+			<MemberOverflowDialog open={isMemberOverflowDialogOpen} onOpenChange={setIsMemberOverflowDialogOpen} />
 			<Nav title={title} />
 			<div className="w-full flex flex-col gap-[15px] px-[15px] custom-shadow-large">
 				{userList.map((user) => (
