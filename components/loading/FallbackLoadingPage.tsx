886abae31e25810b4864b4da9f0acc732ce8e6e9
ShinYoung-Kim <lukey0515@naver.com>
2024-04-26 12:57:47 +0900

    feat: fallback loading 페이지 생성

---
+++ components/loading/FallbackLoadingPage.tsx
@@ -0,0 +1,9 @@
+const FallbackLoadingPage = () => {
+	return (
+		<div className="w-full h-full">
+			<h1 className="typo-Header1">페이지가 로딩중입니다</h1>
+		</div>
+	);
+};
+
+export default FallbackLoadingPage;
