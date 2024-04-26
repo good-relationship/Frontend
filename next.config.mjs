e2a562c2e71ad6e598c3bbbf9f6f0b3f206abd61
ShinYoung-Kim <lukey0515@naver.com>
2024-04-26 12:59:37 +0900

    chore: next 설정 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
+import { env } from 'process';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	reactStrictMode: true,
+	swcMinify: true,
+	rewrites: async () => {
+		return env.NODE_ENV === 'development'
+			? [
+					{
+						source: '/:path*',
+						destination: 'http://localhost:8080/:path*',
+					},
+				]
+			: [];
+	},
+};
 
 export default nextConfig;
