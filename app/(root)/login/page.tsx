9c08d79e505fb13843c82b1cded10dc6aacb8654
ShinYoung-Kim <lukey0515@naver.com>
2024-04-30 10:40:11 +0900

    feat: 로그인 페이지 생성

--- app/(root)/login/page.tsx
+++ app/(root)/login/page.tsx
@@ -1,5 +1,32 @@
+'use client';
+
+import { useSearchParams } from 'next/navigation';
+
 const Login = () => {
-	return <div>로그인 페이지</div>;
+	const searchParams = useSearchParams();
+	const inviteCode = searchParams.get('inviteCode');
+	console.log(inviteCode);
+
+	const handleKakaoLogin = () => {
+		const kakaoLoginUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_LOGIN_REDIRECT_URI}&response_type=code&state=${inviteCode}`;
+		window.location.href = kakaoLoginUrl;
+	};
+
+	const handleNaverLogin = () => {
+		console.log('네이버 로그인');
+	};
+
+	return (
+		<div className="h-full justify-center flex flex-col gap-5">
+			로그인 페이지
+			<button className="bg-Gray-500 text-white" onClick={handleKakaoLogin}>
+				카카오 로그인
+			</button>
+			<button className="bg-Gray-500 text-white" onClick={handleNaverLogin}>
+				네이버 로그인
+			</button>
+		</div>
+	);
 };
 
 export default Login;
