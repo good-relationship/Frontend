9ec38cbb761c2c6d8a7920bb3d355046d673eb4e
ShinYoung-Kim <lukey0515@naver.com>
2024-05-02 13:20:41 +0900

    feat: workspace 이름 및 학교 이름 가져오는 함수 변경

--- hooks/onboarding.ts
+++ hooks/onboarding.ts
@@ -1,22 +1,15 @@
 import { useSearchParams } from 'next/navigation';
 
-export const useCompleteOnboarding = () => {
+export const useGetOnboardingInfoFromSearchParams = () => {
 	const searchParams = useSearchParams();
 
-	const requestCompleteOnboarding = () => {
-		const schoolName = searchParams.get('school');
-		const workspaceName = searchParams.get('workspace');
-		// TODO: API 연결하기
-		console.log(`Completing onboarding for ${schoolName} - ${workspaceName}`);
-	};
-
 	const getSchoolName = () => {
-		return searchParams.get('school');
+		return searchParams.get('school') || '';
 	};
 
 	const getWorkspaceName = () => {
-		return searchParams.get('workspace');
+		return searchParams.get('workspace') || '';
 	};
 
-	return { requestCompleteOnboarding, getSchoolName, getWorkspaceName };
+	return { getSchoolName, getWorkspaceName };
 };
