038c68eb701c38584785618c45c7cc832ee87e0e
ShinYoung-Kim <lukey0515@naver.com>
2024-05-02 17:38:54 +0900

    feat: workspace state에 따라 url 매칭 구현

--- hooks/onboarding.ts
+++ hooks/onboarding.ts
@@ -1,5 +1,8 @@
 import { useSearchParams } from 'next/navigation';
 
+import { URLS } from '@/constants/routings';
+import { WorkspaceState } from '@/models/onboarding/entity/onboarding';
+
 export const useGetOnboardingInfoFromSearchParams = () => {
 	const searchParams = useSearchParams();
 
@@ -12,4 +15,15 @@ export const useGetOnboardingInfoFromSearchParams = () => {
 	};
 
 	return { getSchoolName, getWorkspaceName };
+};
+
+const afterLoginPathMatchingByWorkspaceState = {
+	hasWorkSpace: URLS.WORKSPACE,
+	noSpace: URLS.ONBOARDING_EMPTY,
+	invited: URLS.ONBOARDING_INVITED,
+	overflow: URLS.ONBOARDING_OVERFLOW,
+};
+
+export const useGetAfterLoginPathByWorkspaceState = (workspaceState: WorkspaceState) => {
+	return afterLoginPathMatchingByWorkspaceState[workspaceState];
 };
