83b8e96539d16e18a9af5ebd0140d3b221a4f6e9
ShinYoung-Kim <lukey0515@naver.com>
2024-05-03 16:55:18 +0900

    feat: 초대받기 기능 추가

---
+++ app/(root)/invite/page.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
+
+import { invitedToWorkspace } from '@/apis/workspace';
+import { useAuth } from '@/hooks/auth';
+import { useGetAfterLoginPathByWorkspaceState } from '@/hooks/onboarding';
+
+const InvitePage = () => {
+	const searchParams = useSearchParams();
+	const inviteCode = searchParams.get('inviteCode') || '';
+	const router = useRouter();
+	const { useIsLoggedIn } = useAuth();
+
+	const inviteUserAndNavigate = async () => {
+		const { spaceState } = await invitedToWorkspace(inviteCode);
+		const redirectUrl = useGetAfterLoginPathByWorkspaceState(spaceState || 'noSpace');
+		router.push(redirectUrl);
+	};
+
+	const navigateToLoginWithInviteCode = () => {
+		router.push(`/login?inviteCode=${inviteCode}`);
+	};
+
+	useEffect(() => {
+		if (useIsLoggedIn()) {
+			inviteUserAndNavigate();
+		} else {
+			navigateToLoginWithInviteCode();
+		}
+	}, []);
+
+	return <div>로딩중입니다</div>;
+};
+
+export default InvitePage;
