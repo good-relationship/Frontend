7f5eae2f03519a3d85d40926924d01b11c770b06
ShinYoung-Kim <lukey0515@naver.com>
2024-05-03 10:25:38 +0900

    feat: url 코드 분리 반영

--- apis/search.ts
+++ apis/search.ts
@@ -1,8 +1,8 @@
-import { API_URLS } from '@/constants/routings';
+import { useGetUrl } from '@/hooks/url';
 
 export const searchSchoolName = async (schoolName: string) => {
-	const { SEARCH_SCHOOL } = API_URLS;
-	const response = await fetch(SEARCH_SCHOOL(schoolName));
+	const { getSearchSchoolUrl } = useGetUrl();
+	const response = await fetch(getSearchSchoolUrl(schoolName));
 	if (!response.ok) {
 		// TODO: 검색 에러 처리
 		throw new Error('검색 에러');
