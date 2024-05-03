9302eb5183658052e5ad743bece4541af8da3d2c
ShinYoung-Kim <lukey0515@naver.com>
2024-05-03 18:38:54 +0900

    fix: 타입 변경 미반영 수정

--- mocks/onboarding.ts
+++ mocks/onboarding.ts
@@ -1,30 +1,28 @@
 import { GetUserWorkspaceInfoResponseDTO } from '@/models/onboarding/response/getWorkspaceUserInfoResponseDTO';
 
-export const mockGetUserWorkspaceInfoData: GetUserWorkspaceInfoResponseDTO = {
-	users: [
-		{
-			userId: 1,
-			userName: '김코딩',
-			email: 'abc@naver.com',
-			userImage: 'https://via.placeholder.com/45',
-		},
-		{
-			userId: 2,
-			userName: '박디자인',
-			email: 'abc@naver.com',
-			userImage: 'https://via.placeholder.com/45',
-		},
-		{
-			userId: 3,
-			userName: '이테스터',
-			email: 'abc@naver.com',
-			userImage: 'https://via.placeholder.com/45',
-		},
-		{
-			userId: 4,
-			userName: '최마케터',
-			email: 'abc@naver.com',
-			userImage: 'https://via.placeholder.com/45',
-		},
-	],
-};
+export const mockGetUserWorkspaceInfoData: GetUserWorkspaceInfoResponseDTO = [
+	{
+		userId: 1,
+		userName: '김코딩',
+		email: 'abc@naver.com',
+		userImage: 'https://via.placeholder.com/45',
+	},
+	{
+		userId: 2,
+		userName: '박디자인',
+		email: 'abc@naver.com',
+		userImage: 'https://via.placeholder.com/45',
+	},
+	{
+		userId: 3,
+		userName: '이테스터',
+		email: 'abc@naver.com',
+		userImage: 'https://via.placeholder.com/45',
+	},
+	{
+		userId: 4,
+		userName: '최마케터',
+		email: 'abc@naver.com',
+		userImage: 'https://via.placeholder.com/45',
+	},
+];
