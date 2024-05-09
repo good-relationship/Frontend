2a507b8edea768a318d3cb3a9cfd24c74c95e76f
ShinYoung-Kim <lukey0515@naver.com>
2024-05-10 04:34:23 +0900

    feat: 로그인 페이지 컨텐츠 추가

--- app/(root)/login/page.tsx
+++ app/(root)/login/page.tsx
@@ -10,8 +10,9 @@ const Login = () => {
 	const inviteCode = searchParams.get('inviteCode') || '';
 
 	return (
-		<div className="h-full justify-center flex flex-col gap-5">
-			로그인 페이지
+		<div className="h-full justify-center flex flex-col gap-5 w-full items-center">
+			<h1 className="typo-Header1">로그인</h1>
+			<h6 className="typo-Body1 text-Gray-400">계정에 로그인하시고 협업을 시작해보세요</h6>
 			<KakaoLoginButton inviteCode={inviteCode} />
 			<NaverLoginButton inviteCode={inviteCode} />
 		</div>
