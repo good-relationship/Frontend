a1a654916a5abc66760e526a08bd697407bc4a75
ShinYoung-Kim <lukey0515@naver.com>
2024-05-10 04:31:58 +0900

    feat: sns 로그인 버튼 분리 및 스타일 적용

--- app/(root)/login/page.tsx
+++ app/(root)/login/page.tsx
@@ -2,29 +2,18 @@
 
 import { useSearchParams } from 'next/navigation';
 
+import KakaoLoginButton from '@/components/login/KakaoLoginButton';
+import NaverLoginButton from '@/components/login/NaverLoginButton';
+
 const Login = () => {
 	const searchParams = useSearchParams();
-	const inviteCode = searchParams.get('inviteCode');
-
-	const handleKakaoLogin = () => {
-		const kakaoLoginUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_LOGIN_REDIRECT_URI}&response_type=code&state=${inviteCode}`;
-		window.location.href = kakaoLoginUrl;
-	};
-
-	const handleNaverLogin = () => {
-		const naverLoginUrl = `https://nid.naver.com/oauth2.0/authorize?client_id=${process.env.NEXT_PUBLIC_NAVER_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_NAVER_LOGIN_REDIRECT_URI}&response_type=code&state=${inviteCode}`;
-		window.location.href = naverLoginUrl;
-	};
+	const inviteCode = searchParams.get('inviteCode') || '';
 
 	return (
 		<div className="h-full justify-center flex flex-col gap-5">
 			로그인 페이지
-			<button className="bg-Gray-500 text-white" onClick={handleKakaoLogin}>
-				카카오 로그인
-			</button>
-			<button className="bg-Gray-500 text-white" onClick={handleNaverLogin}>
-				네이버 로그인
-			</button>
+			<KakaoLoginButton inviteCode={inviteCode} />
+			<NaverLoginButton inviteCode={inviteCode} />
 		</div>
 	);
 };
---
+++ components/login/KakaoLoginButton.tsx
@@ -0,0 +1,25 @@
+'use clinet';
+
+import Image from 'next/image';
+
+import RoundedButton from '@/components/RoundedButton';
+
+type KakaoLoginButtonProps = {
+	inviteCode: string;
+};
+
+const KakaoLoginButton = ({ inviteCode }: KakaoLoginButtonProps) => {
+	const handleKakaoLogin = () => {
+		const kakaoLoginUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_LOGIN_REDIRECT_URI}&response_type=code&state=${inviteCode}`;
+		window.location.href = kakaoLoginUrl;
+	};
+
+	return (
+		<RoundedButton className="bg-Kakao" onClick={handleKakaoLogin} size="Large">
+			<Image src="/icons/kakao.svg" width={28} height={28} alt="kakao icon" />
+			<h6 className="typo-SubHeader3 text-black">카카오로 시작하기</h6>
+		</RoundedButton>
+	);
+};
+
+export default KakaoLoginButton;
