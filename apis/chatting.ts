07cd82a8cf686f2d01de8cfe991459d994a56e47
Imjieunn <jieun776121@gmail.com>
2024-05-15 11:55:31 +0900

    refactor: 메세지 구독 함수 분리

--- apis/chatting.ts
+++ apis/chatting.ts
@@ -1,28 +1,26 @@
 import { Client } from "@stomp/stompjs";
 import { MutableRefObject } from "react";
 
-// import { useAddMessage } from "@/hooks/addMessage";
+import { GetMessageContentDTO } from "@/models/chatting/response/getMessageContentDTO";
+import { GetMessageHistoryDTO } from "@/models/chatting/response/getMessageHistoryDTO";
 
-// export const subscribeToMessageTopic = (newClient: Client, workspaceId: string, headers: {Authorization: string;}) => {
-//     newClient.subscribe(`/topic/message/${workspaceId}`, (message) => {
-//         const messageObj = JSON.parse(message.body).body;
-//         console.log(messageObj);
-//     }, headers);
-// };
 
-export const subscribeToHistoryTopic = (newClient: Client, headers: {Authorization: string;}) => {
-    newClient.subscribe(`/user/topic/history`, (history) => {
-        console.log('subscribe to history topic');
-        const messageObj = JSON.parse(history.body).body;
-        console.log(messageObj);
-        // useAddMessageBeforeToList(messageObj.messages);
-        // useMessageHistoryState({
-        //     start: messageObj.start,
-        //     end: messageObj.end,
-        //     lastMsgId: messageObj.lastMsgId
-        // });
+export const subscribeToMessageTopic = (client: Client, headers: { Authorization: string; }, workspaceId:string, onMessageReceivedCallback: (arg0:GetMessageContentDTO) => void) => {
+    client?.subscribe(`/topic/message/${workspaceId}`, (message) => {
+        const messageObj = JSON.parse(message.body).body;
+        onMessageReceivedCallback(messageObj);
     }, headers);
 };
+
+export const subscribeToHistoryTopic = (client: Client, headers: { Authorization: string; }, onHistoryReceivedCallback: (arg0: GetMessageHistoryDTO, arg1: GetMessageContentDTO[]) => void) => {
+    client?.subscribe(`/user/queue/history`, (history) => {
+        const messageObj = JSON.parse(history.body).body;
+        const message = messageObj.messages;
+        const messageInfo = messageObj
+        onHistoryReceivedCallback(messageInfo, message);
+    }, headers);
+};
+
 
 export const sendMessageApi = (client: MutableRefObject<Client | null>, messageContent:string, getWorkspaceId:string) => {
     client.current?.publish({
