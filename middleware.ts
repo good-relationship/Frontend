fd5ab9ba75eb5fee1ec8d47bc96c2135d81aee16
ShinYoung-Kim <lukey0515@naver.com>
2024-05-26 15:20:04 +0900

    fix: auth 관련 에러 제거

--- middleware.ts
+++ middleware.ts
@@ -22,11 +22,11 @@ export async function middleware(request: NextRequest) {
 		}
 	}
 
-	const headers = new Headers(request.headers);
-	headers.set('x-current-path', request.nextUrl.pathname);
-	return NextResponse.next({ headers });
+	// const headers = new Headers(request.headers);
+	// headers.set('x-current-path', request.nextUrl.pathname);
+	// return NextResponse.next({ headers });
 }
 
-export const config = {
-	matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
-};
+// export const config = {
+// 	matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+// };
