1f6cb7d61e06eed8eb9fce1b3366dc7bf844d280
Imjieunn <jieun776121@gmail.com>
2024-05-31 14:25:24 +0900

    fix: 채팅 플로팅 버튼 workspace에서만 나타나게 변경

--- app/(root)/(workspace)/layout.tsx
+++ app/(root)/(workspace)/layout.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from 'react';
 
+import FloatingInfo from '@/components/chatting/FloatingInfo';
 import Sidebar from '@/components/sidebar/Sidebar';
 
 const WorkspaceLayout = ({
@@ -11,6 +12,9 @@ const WorkspaceLayout = ({
 		<div className="flex h-full">
 			<Sidebar />
 			<section className="p-[72px]">{children}</section>
+			<div className="fixed bottom-0 right-10 p-4">
+				<FloatingInfo />
+			</div>
 		</div>
 	);
 };
