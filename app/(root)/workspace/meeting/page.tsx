daaf304fc5879a2c096d2e3f41bae0b59e6eeea8
ShinYoung-Kim <lukey0515@naver.com>
2024-06-04 00:05:24 +0900

    feat: meeting web socket api 변경 반영

--- app/(root)/workspace/meeting/page.tsx
+++ app/(root)/workspace/meeting/page.tsx
@@ -1,11 +1,11 @@
 'use client';
 
-import { Client } from '@stomp/stompjs';
 import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 
 import { getWorkspaceInfo } from '@/apis/workspace';
 import MeetingItem from '@/components/meeting/waitingRoom/MeetingItem';
+import { useGetAccessToken } from '@/hooks/auth';
 import { useWebsocket } from '@/lib/websocket/WebsocketProvider';
 import { RoomList } from '@/models/meeting/entity/meeting';
 
@@ -20,59 +20,87 @@ const MeetingPage = () => {
 				return;
 			}
 			const { workspaceId } = await getWorkspaceInfo();
-			subscribeMeetingList.bind(null, stompClient, workspaceId)();
-			publishMeetingList.bind(null, stompClient)();
+			subscribeMeetingList(workspaceId);
+			getMeetingRoomList();
 		};
 
 		handleConnect();
 	}, [stompClient]);
 
-	const subscribeMeetingList = (client: Client, workspaceId: string) => {
-		client.subscribe(`/topic/${workspaceId}/meetingRoomList`, function (message) {
+	const subscribeMeetingList = (workspaceId: string) => {
+		stompClient.subscribe(`/topic/${workspaceId}/meetingRoomList`, function (message) {
 			const body = JSON.parse(message.body);
 			setRoomList(body.meetingRoomList);
 		});
 	};
 
-	const publishMeetingList = (client: Client) => {
-		client.publish({
-			destination: '/app/room/list',
+	const getMeetingRoomList = async () => {
+		const accessToken = await useGetAccessToken();
+		const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/meet/room/list`, {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${accessToken}`,
+			},
 		});
+
+		if (!response.ok) {
+			throw new Error('회의 목록 조회 오류');
+		}
 	};
 
-	const createMeeting = () => {
-		if (!stompClient || !stompClient.connected) {
-			return;
-		}
-
-		stompClient.publish({
-			destination: `/app/room/create`,
+	const createMeeting = async () => {
+		const accessToken = await useGetAccessToken();
+		const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/meet/room/create`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${accessToken}`,
+			},
 			body: JSON.stringify({
-				roomName: 'test',
+				roomName: '새 회의',
 			}),
 		});
-	};
 
-	const joinMeeting = (roomId: string) => {
-		if (!stompClient || !stompClient.connected) {
-			return;
+		if (!response.ok) {
+			throw new Error('회의 나가기 오류');
 		}
 
-		stompClient.publish({
-			destination: `/app/room/join/${roomId}`,
-		});
+		const { roomId } = await response.json();
 
 		router.push(`/workspace/meeting/${roomId}`);
 	};
 
-	const exitMeeting = () => {
-		if (!stompClient || !stompClient.connected) {
-			return;
+	const joinMeeting = async (roomId: string) => {
+		const accessToken = await useGetAccessToken();
+		const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/meet/room/join/${roomId}`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${accessToken}`,
+			},
+		});
+
+		if (!response.ok) {
+			throw new Error('회의 참여하기 오류');
 		}
 
-		stompClient.publish({
-			destination: `/app/room/leave`,
+		router.push(`/workspace/meeting/${roomId}`);
+	};
+
+	const exitMeeting = async () => {
+		const accessToken = await useGetAccessToken();
+		const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/meet/room/leave`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${accessToken}`,
+			},
 		});
+
+		if (!response.ok) {
+			throw new Error('회의 나가기 오류');
+		}
 	};
 
 	return (
