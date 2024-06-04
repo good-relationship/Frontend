1bfacd4945b0290241644868de25c57f303a7800
ShinYoung-Kim <lukey0515@naver.com>
2024-06-04 21:45:41 +0900

    feat: local stream 설정 추가

--- app/(root)/workspace/meeting/[meetingId]/page.tsx
+++ app/(root)/workspace/meeting/[meetingId]/page.tsx
@@ -1,5 +1,28 @@
+'use client';
+
+import { useEffect, useRef } from 'react';
+
 const MeetingRoomPage = ({ params }: { params: { meetingId: string } }) => {
-	return <div>회의실 번호 : {params.meetingId}</div>;
+	const localVideoRef = useRef<HTMLVideoElement>(null);
+	const localStreamRef = useRef<MediaStream>();
+	const setLocalStream = async () => {
+		const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+		localStreamRef.current = stream;
+		if (localVideoRef.current) {
+			localVideoRef.current.srcObject = stream;
+		}
+	};
+
+	useEffect(() => {
+		setLocalStream();
+	}, []);
+
+	return (
+		<div>
+			회의실 번호 : {params.meetingId}
+			<video ref={localVideoRef} autoPlay muted />
+		</div>
+	);
 };
 
 export default MeetingRoomPage;
