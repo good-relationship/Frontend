846f17dc81213d28a1827fb8896e1e5734ddd725
ShinYoung-Kim <lukey0515@naver.com>
2024-06-06 01:40:07 +0900

    feat: url util 함수 생성

---
+++ utils/url.ts
@@ -0,0 +1,16 @@
+import { ApiUrlType } from '@/constants/url';
+
+export const getUrl =
+	(baseUrl: string = '') =>
+	(path: ApiUrlType) =>
+	(params: Record<string, string> = {}) => {
+		const url = new URL(path, baseUrl);
+		Object.keys(params).forEach((key) => url.searchParams.append(key, params[key]));
+		return url.toString();
+	};
+
+export const getApiUrl = getUrl(process.env.NEXT_PUBLIC_API_URL);
+export const getPublicUrl = getUrl(process.env.NEXT_PUBLIC_URL);
+
+export const isWorkspaceUrl = (url: string) => url.startsWith('/workspace');
+export const isOnboardingUrl = (url: string) => url.startsWith('/onboarding');
