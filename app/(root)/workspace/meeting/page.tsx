54ed736a345b117738540c998333cdce9a1d61d5
ShinYoung-Kim <lukey0515@naver.com>
2024-06-06 22:37:45 +0900

    feat: 회의 페이지 header layout 생성

--- app/(root)/workspace/meeting/page.tsx
+++ app/(root)/workspace/meeting/page.tsx
@@ -6,6 +6,7 @@ import { useEffect, useState } from 'react';
 import { createMeeting, getMeetingRoomList, joinMeeting } from '@/apis/meeting';
 import { getWorkspaceInfo } from '@/apis/workspace';
 import MeetingItem from '@/components/meeting/waitingRoom/MeetingItem';
+import RoundedButton from '@/components/RoundedButton';
 import { useWebsocket } from '@/lib/websocket/WebsocketProvider';
 import { RoomList } from '@/models/meeting/entity/meeting';
 
@@ -45,9 +46,18 @@ const MeetingPage = () => {
 	};
 
 	return (
-		<div>
-			여기는 회의 페이지
-			<button onClick={handleCreateMeeting}>새 회의 생성</button>
+		<div className="w-full">
+			<div>
+				<h1 className="typo-Header6 text-Gray-400">서울과학기술대학교</h1>
+				<h3 className="typo-Header1">워크스페이스 이름</h3>
+			</div>
+			<hr className="my-3 h-4" />
+			<div className="w-full flex justify-between">
+				<h6 className="typo-SubHeader1">진행중인 회의</h6>
+				<RoundedButton variant="Outline" size="Small" onClick={handleCreateMeeting}>
+					새 회의 생성
+				</RoundedButton>
+			</div>
 			{roomList && (
 				<div>
 					{roomList.map((room) => {
