73361a91964b8d3de228d08ccb3eb047c489137f
ShinYoung-Kim <lukey0515@naver.com>
2024-06-06 23:24:44 +0900

    feat: form scheme 및 type 추가

--- constants/form.ts
+++ constants/form.ts
@@ -4,4 +4,9 @@ export const workspaceNameSchema = z.object({
 	workspaceName: z.string().min(1, '필수 입력 사항입니다.').max(10, '10자 이내로 입력해주세요'),
 });
 
+export const meetingNameSchema = z.object({
+	meetingName: z.string().min(1, '필수 입력 사항입니다.').max(10, '10자 이내로 입력해주세요'),
+});
+
 export type WorkspaceNameSchemaType = typeof workspaceNameSchema;
+export type MeetingNameSchemaType = typeof meetingNameSchema;
