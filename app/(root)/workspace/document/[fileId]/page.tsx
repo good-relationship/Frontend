0d786a9c2c4747e9c36deb7ce990ea2d954c0230
jieunn <jieun776121@gmail.com>
2024-06-10 15:12:53 +0900

    fix: 상세 문서 페이지 경로 변경

---
+++ app/(root)/workspace/document/[fileId]/page.tsx
@@ -0,0 +1,12 @@
+import FileHeader from '@/components/document/FileHeader';
+
+const FilePage = ({ params }: { params: { fileId: string } }) => {
+	return (
+		<>
+			<div>파일 번호 : {params.fileId}</div>
+			<FileHeader />
+		</>
+	);
+};
+
+export default FilePage;
