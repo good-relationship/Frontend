bf08434143410978e1048ed834573be747655684
jieunn <jieun776121@gmail.com>
2024-06-13 01:09:15 +0900

    feat : file에 blocknote editor 추가

--- components/document/FileContent.tsx
+++ components/document/FileContent.tsx
@@ -3,11 +3,24 @@
 import Editor from './editor';
 
 const FileContent = () => {
+	const temp = [
+		{
+			type: 'paragraph',
+			content: 'Hello, world!',
+		},
+		{
+			type: 'image',
+			src: 'image.jpg',
+		},
+	];
+
+	const data = JSON.stringify(temp);
+
 	return (
-		<>
-			<div className="mt-14 typo-Header5 text-gray-200">내용을 입력하세요.</div>
-			<Editor onChange={() => {}} />
-		</>
+		<div className="mt-8">
+			{/* onChange에는 변화하는 document 넣기 (update) */}
+			<Editor onChange={() => {}} initialContent={data} />
+		</div>
 	);
 };
 
