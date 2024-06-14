a3de49f090646ff52158c9480d7683e4afcfbb79
ShinYoung-Kim <lukey0515@naver.com>
2024-06-14 10:25:48 +0900

    design: 아이콘 버튼 색상 변경

--- components/meeting/meetingRoom/IconButton.tsx
+++ components/meeting/meetingRoom/IconButton.tsx
@@ -11,8 +11,11 @@ export interface IconButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>
 
 const IconButton = ({ icon, onClick }: IconButtonProps) => {
 	return (
-		<button className="bg-Purple-600 w-10 h-10 flex justify-center items-center rounded-full" onClick={onClick}>
-			<Icon id={icon} color={colors['Purple-100']} size={16} />
+		<button
+			className="bg-Gray-400 text-Gray-100 opacity-75 w-10 h-10 flex justify-center items-center rounded-full"
+			onClick={onClick}
+		>
+			<Icon id={icon} color={colors['Gray-100']} size={16} />
 		</button>
 	);
 };
