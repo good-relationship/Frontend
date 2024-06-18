a773a5c1c002513f6015fd6018d6cc316b0e0e44
ShinYoung-Kim <lukey0515@naver.com>
2024-06-18 09:28:25 +0900

    feat: icon 버튼 개별 버튼으로 분리

--- components/meeting/meetingRoom/ButtonGroup.tsx
+++ components/meeting/meetingRoom/ButtonGroup.tsx
@@ -1,55 +1,26 @@
 'use client';
 
-import { leaveMeeting } from '@/apis/meeting';
-import IconButton from '@/components/meeting/meetingRoom/IconButton';
-import { IconId } from '@/types/icons';
+import { ReactNode } from 'react';
+
+import ControlCameraButton from '@/components/meeting/meetingRoom/iconButton/ControlCameraButton';
+import ControlMicrophoneButton from '@/components/meeting/meetingRoom/iconButton/ControlMicrophoneButton';
+import ControlScreenButton from '@/components/meeting/meetingRoom/iconButton/ControlScreenButton';
+import ExitMeetingButton from '@/components/meeting/meetingRoom/iconButton/ExitMeetingButton';
+import OpenDocumentButton from '@/components/meeting/meetingRoom/iconButton/OpenDocumentButton';
+import OpenWhiteBoardButton from '@/components/meeting/meetingRoom/iconButton/OpenWhiteBoradButton';
 
 const ButtonGroup = () => {
-	type IconButtonConfig = {
-		id: string;
-		icon: IconId;
-		onClick: () => void;
-	};
-
-	const iconButtonConfig: IconButtonConfig[] = [
-		{
-			id: 'camera',
-			icon: 'webcam',
-			onClick: () => console.log('카메라 클릭'),
-		},
-		{
-			id: 'microphone',
-			icon: 'microphone',
-			onClick: () => console.log('마이크 클릭'),
-		},
-		{
-			id: 'screen',
-			icon: 'screen',
-			onClick: () => console.log('화면 공유 클릭'),
-		},
-		{
-			id: 'document',
-			icon: 'pencil',
-			onClick: () => console.log('문서 클릭'),
-		},
-		{
-			id: 'whiteboard',
-			icon: 'palette',
-			onClick: () => console.log('화이트보드 클릭'),
-		},
-		{
-			id: 'exit',
-			icon: 'exit',
-			onClick: leaveMeeting,
-		},
+	const iconButtons: ReactNode[] = [
+		<ControlCameraButton key="camera" />,
+		<ControlMicrophoneButton key="microphone" />,
+		<ControlScreenButton key="screen" />,
+		<OpenDocumentButton key="document" />,
+		<OpenWhiteBoardButton key="whiteboard" />,
+		<ExitMeetingButton key="exit" />,
 	];
 
 	return (
-		<section className="w-full flex justify-center gap-5">
-			{iconButtonConfig.map((config) => {
-				return <IconButton key={config.id} icon={config.icon} onClick={config.onClick} />;
-			})}
-		</section>
+		<section className="w-full flex justify-center gap-5">{iconButtons.map((iconButton) => iconButton)}</section>
 	);
 };
 
