3ec0e02bdb57c245422360eb2ec88b2cf062dbc4
jieunn <jieun776121@gmail.com>
2024-06-18 15:13:04 +0900

    fix: radix 상태 제어 수정

--- components/chatting/FloatingButton.tsx
+++ components/chatting/FloatingButton.tsx
@@ -5,11 +5,9 @@ import ChattingPop from './ChattingPop';
 import { Popover, PopoverTrigger } from '@/components/ui/popover';
 
 const FloatingButton = () => {
-	// const [isPopoverOpen, setIsPopoverOpen] = useState(false);
-
 	return (
 		<Popover>
-			<PopoverTrigger className="md:block md:radix-state-open:block hidden">
+			<PopoverTrigger className="block radix-state-open:hidden md:radix-state-open:block">
 				<img src="/icons/chatting.svg" alt="채팅아이콘" />
 			</PopoverTrigger>
 			<ChattingPop />
