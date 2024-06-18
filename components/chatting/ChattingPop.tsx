92413b9225f74c989f1505fec94c617adaf33383
jieunn <jieun776121@gmail.com>
2024-06-18 14:31:35 +0900

    style: vw, vh 단위 screen으로 변경

--- components/chatting/ChattingPop.tsx
+++ components/chatting/ChattingPop.tsx
@@ -8,7 +8,7 @@ const ChattingPop = () => {
 	return (
 		<PopoverContent
 			align="end"
-			className="w-[100vw] h-[100vh] md:w-[370px] md:h-[75vh] md:max-h-[720px] md:block py-0 z-0 text-black translate-y-[-14px]"
+			className="w-screen h-screen md:w-[370px] md:h-[75vh] md:max-h-[720px] md:block py-0 z-0 text-black translate-y-[-14px]"
 		>
 			<Chatting>
 				<PopoverClose>
