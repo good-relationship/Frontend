caf3192963b889744d2a7bb472f5f89bc4dc7a5e
jieunn <jieun776121@gmail.com>
2024-06-27 14:45:57 +0900

    feat: 사이드바 상태 recoil 전역 관리

--- components/sidebar/SidebarLink.tsx
+++ components/sidebar/SidebarLink.tsx
@@ -1,8 +1,10 @@
 import Link from 'next/link';
+import { useRecoilState } from 'recoil';
 
 import Icon from '@/components/Icon';
 import { colors } from '@/constants/colors';
 import { cn } from '@/lib/utils';
+import { getMenuButtonState } from '@/stores/atoms/getMenuButton';
 import { IconId } from '@/types/icons';
 
 type SidebarLinkProps = {
@@ -13,12 +15,18 @@ type SidebarLinkProps = {
 };
 
 const SidebarLink = ({ label, path, icon, isActive }: SidebarLinkProps) => {
+	const [sidebarOpen, setSidebarOpen] = useRecoilState(getMenuButtonState);
+	const controlSidebar = () => {
+		setSidebarOpen({ isOpen: !sidebarOpen.isOpen });
+	};
+
 	return (
 		<Link
 			href={path}
 			className={cn('flex gap-[10px] rounded-[10px] typo-SubHeader3 px-5 py-[15px]', {
 				'!text-Purple-500 bg-Purple-100': isActive,
 			})}
+			onClick={controlSidebar}
 		>
 			<Icon id={icon} color={isActive ? colors['Purple-500'] : colors['Gray-400']} size={20} />
 			<span className="block sm:hidden lg:block">{label}</span>
