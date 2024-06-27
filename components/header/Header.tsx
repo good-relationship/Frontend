0df311e362802fbe572319ca62ced780dde3a976
jieunn <jieun776121@gmail.com>
2024-06-27 14:46:43 +0900

    feat: 모바일 크기 진입 시 사이드바 숨기기

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,6 +1,6 @@
-import { MenuIcon } from 'lucide-react';
 import { headers } from 'next/headers';
-import Image from 'next/image';
+
+import SidebarButton from './SidebarButton';
 
 import UserInfo from '@/components/header/UserInfo';
 import { UrlType } from '@/constants/url';
@@ -13,16 +13,7 @@ const Header = () => {
 
 	return (
 		<header className="fixed top-0 h-[60px] w-full bg-black flex px-[30px] sm:px-[60px] justify-between">
-			<div className="gap-4 flex">
-				<MenuIcon color="white" className="block sm:hidden h-auto w-8" />
-				<Image
-					src="/icons/kan_text_horizontal.svg"
-					alt="조은사이 가로 로고"
-					width="0"
-					height="0"
-					className="w-[80px] sm:w-[150px] h-auto"
-				/>
-			</div>
+			<SidebarButton />
 			{!isPlainHeader && <UserInfo />}
 		</header>
 	);
