2d02939bf6c32afc6db2bfa10a6bc7bcc4463779
ShinYoung-Kim <lukey0515@naver.com>
2024-11-29 10:16:04 +0900

    feat: canvas 및 hook 코드 분리

--- components/canvas/Canvas.tsx
+++ components/canvas/Canvas.tsx
@@ -1,80 +1,14 @@
 'use client';
 
-import { LiveObject } from '@liveblocks/client';
 import { useSelf } from '@liveblocks/react';
-import { useMutation } from '@liveblocks/react/suspense';
-import { PointerEvent, useCallback, useState, WheelEvent } from 'react';
 
 import Path from '@/components/canvas/Path';
 import PathList from '@/components/canvas/PathList';
-import { getPathFromPoints, getPoint } from '@/components/canvas/point.util';
-import { Point } from '@/types/whiteboard';
+import { useCanvas } from '@/components/canvas/useCanvas';
 
 const Canvas = () => {
 	const pencilDraft = useSelf((me) => me.presence.pencilDraft);
-	const [camera, setCamera] = useState<Point>({ x: 0, y: 0 });
-
-	const startDrawing = useMutation(({ setMyPresence }, point: Point, pressure: number) => {
-		setMyPresence({
-			pencilDraft: [[point.x, point.y, pressure]],
-			penColor: '#000000',
-		});
-	}, []);
-
-	const draw = useMutation(({ self, setMyPresence }, point: Point, pressure: number) => {
-		const { pencilDraft } = self.presence;
-
-		setMyPresence({
-			cursor: point,
-			pencilDraft: pencilDraft ? [...pencilDraft, [point.x, point.y, pressure]] : [[point.x, point.y, pressure]],
-		});
-	}, []);
-
-	const changeDraftIntoLayer = useMutation(({ storage, self, setMyPresence }) => {
-		const liveLayers = storage.get('layers');
-		const { pencilDraft } = self.presence;
-		const id = Date.now().toString();
-
-		if (pencilDraft == null) {
-			setMyPresence({ pencilDraft: null });
-			return;
-		}
-
-		liveLayers.set(id, new LiveObject(getPathFromPoints(pencilDraft)));
-		const layerOrderList = storage.get('layerOrderList');
-		layerOrderList.push(id);
-		setMyPresence({ pencilDraft: null });
-	}, []);
-
-	const onPointerLeave = useMutation(({ setMyPresence }) => {
-		setMyPresence({ cursor: null });
-	}, []);
-
-	const onWheel = useCallback((e: WheelEvent) => {
-		setCamera((prev) => ({ x: prev.x - e.deltaX, y: prev.y - e.deltaY }));
-	}, []);
-
-	const onPointerDown = useCallback(
-		(e: PointerEvent) => {
-			const point = getPoint(e, camera);
-			startDrawing(point, e.pressure);
-		},
-		[camera, startDrawing],
-	);
-
-	const onPointerMove = useMutation(
-		({ setMyPresence }, e: PointerEvent) => {
-			e.preventDefault();
-			const point = getPoint(e, camera);
-			draw(point, e.pressure);
-			setMyPresence({ cursor: point });
-		},
-		[camera, draw],
-	);
-
-	const onPointerUp = useMutation(() => {
-		changeDraftIntoLayer();
-	}, [changeDraftIntoLayer]);
+	const { onPointerDown, onPointerMove, onPointerUp, onPointerLeave, onWheel, camera } = useCanvas();
 
 	return (
 		<div className="touch-none h-full">
