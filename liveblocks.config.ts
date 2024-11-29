1ff2779dc2bec697e96cb4dadf285cf0c9fcd9da
ShinYoung-Kim <lukey0515@naver.com>
2024-11-29 10:37:47 +0900

    chore: liveblocks initial 타입 정의 파일 변경

--- liveblocks.config.ts
+++ liveblocks.config.ts
@@ -5,13 +5,13 @@ import { Layer, Point } from '@/types/whiteboard';
 declare global {
 	interface Liveblocks {
 		Presence: {
-			cursor: Point | null;
-			pencilDraft: [x: number, y: number, pressure: number][] | null;
-			penColor: string;
+			cursor?: Point | null;
+			pencilDraft?: [x: number, y: number, pressure: number][] | null;
+			penColor?: string;
 		};
 		Storage: {
-			layers: LiveMap<string, LiveObject<Layer>>;
-			layerOrderList: LiveList<string>;
+			layers?: LiveMap<string, LiveObject<Layer>>;
+			layerOrderList?: LiveList<string>;
 		};
 	}
 }
